90565fb6faf790e550872887727d540766196422
tofaccantona17-commits <tofaccantona17@gmail.com>
2026-03-06 14:08:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function downloadCV(){
+
+alert("Upload your CV file and link it here!");
+
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Message sent successfully!");
+
+});
